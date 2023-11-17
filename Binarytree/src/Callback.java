ba7155d1f8d52015284539de1e5274cfabd42e34
John Sebastian <villegassebastian236@gmail.com>
2023-11-17 15:22:27 -0500

    second commit

--- Binarytree/src/Callback.java
+++ Binarytree/src/Callback.java
@@ -5,7 +5,7 @@ interface Callback {
 class SomeCallback implements Callback {
     @Override
     public void execute(BinaryTree node) {
-        // Aquí se llama a la función de devolución de llamada
+        
         System.out.println("callback(" + node.value + ")");
     }
 }
